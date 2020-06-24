77fee5e7bbe79d9bc16903ff755af9cf23791024
myleyuan <67321858+myleyuan@users.noreply.github.com>
2020-06-24 08:28:16 +0800

    Update hy.sh

--- hy.sh
+++ hy.sh
@@ -8,11 +8,14 @@ if [ ! -f go.sh ];then
 else
     echo "即将运行go.sh脚本，终止请按Ctrl+c"  
 fi
+echo -e "\033[?25l"
 for I in {0..4};do
     J=$[5-$I]
     echo -ne "$J\r"
     sleep 1
 done
+
+echo -e "\033[?25h"
 chmod +x go.sh
 ./go.sh
 systemctl start v2ray
@@ -21,12 +24,14 @@ echo "检测加速脚本bbr.sh是否存在"
 if [ ! -f bbr.sh ];then
     echo "未检测到加速脚本bbr.sh！"
 else
-    echo "加速脚本bbr.sh存在，即将开始运行！"
+    echo "加速脚本bbr.sh存在，即将开始运行！"   
+    echo -e "\033[?25l"
     for I in {0..4};do
         J=$[5-$I]
         echo -ne "$J\r"
         sleep 1
     done
+    echo -e "\033[?25h"
     chmod +x bbr.sh
     ./bbr.sh
 fi
