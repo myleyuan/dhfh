6ed867c0358eaa992085331b4d6f54218d3bd6eb
myleyuan <67321858+myleyuan@users.noreply.github.com>
2020-06-28 14:57:34 +0800

    Update hy.sh

--- hy.sh
+++ hy.sh
@@ -19,7 +19,6 @@ echo -e "\033[?25h"
 chmod +x go.sh
 ./go.sh
 systemctl start v2ray
-cat /etc/v2ray/config.json
 echo "检测加速脚本bbr.sh是否存在"
 if [ ! -f bbr.sh ];then
     echo "未检测到加速脚本bbr.sh！"
@@ -36,3 +35,15 @@ else
     ./bbr.sh
 fi
 
+echo "显示v2ray节点信息："
+
+cat /etc/v2ray/config.json
+echo "即将重启服务器，重启后请手动连接服务器"
+echo -e "\033[?25l"
+for I in {0..4};do
+    J=$[5-$I]		   
+    echo -ne "$J\r"		 
+    sleep 1		  
+done
+echo -e "\033[?25h"
+reboot
