32db7fb28465bebf08a77ab5c3d33e207f035761
myleyuan <67321858+myleyuan@users.noreply.github.com>
2020-06-28 15:06:13 +0800

    Update hy.sh

--- hy.sh
+++ hy.sh
@@ -4,9 +4,9 @@ systemctl stop firewalld
 yum install zip unzip -y
 if [ ! -f go.sh ];then
     wget https://install.direct/go.sh
-    echo "即将运行go.sh脚本，终止请按Ctrl+c" 
+    echocolor red "即将运行go.sh脚本，终止请按Ctrl+c" 
 else
-    echo "即将运行go.sh脚本，终止请按Ctrl+c"  
+    echocolor red "即将运行go.sh脚本，终止请按Ctrl+c"  
 fi
 echo -e "\033[?25l"
 for I in {0..4};do
@@ -19,11 +19,11 @@ echo -e "\033[?25h"
 chmod +x go.sh
 ./go.sh
 systemctl start v2ray
-echo "检测加速脚本bbr.sh是否存在"
+echocolor yellow "检测加速脚本bbr.sh是否存在"
 if [ ! -f bbr.sh ];then
-    echo "未检测到加速脚本bbr.sh！"
+    echocolor yellow "未检测到加速脚本bbr.sh！"
 else
-    echo "加速脚本bbr.sh存在，即将开始运行！"   
+    echocolor yellow "加速脚本bbr.sh存在，即将开始运行！"   
     echo -e "\033[?25l"
     for I in {0..4};do
         J=$[5-$I]
@@ -35,10 +35,11 @@ else
     ./bbr.sh
 fi
 
-echo "显示v2ray节点信息："
+echocolor purple "显示v2ray节点信息："
 
 cat /etc/v2ray/config.json
-echo "即将重启服务器，重启后请手动连接服务器"
+sleep 2
+echocolor red "即将重启服务器，重启后请手动连接服务器"
 echo -e "\033[?25l"
 for I in {0..4};do
     J=$[5-$I]		   
