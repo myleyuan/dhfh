c1e257af338fdee47eb4b36ce7298efd2481387a
myleyuan <67321858+myleyuan@users.noreply.github.com>
2020-06-28 17:33:41 +0800

    Update hy.sh

--- hy.sh
+++ hy.sh
@@ -1,49 +1,17 @@
 #!/bin/bash
 
-
-echocolor（）{
-color=${1} && shift
-case ${color} in
-black）
-echo -e “\e［0;30m${@}\e［0m”
-;;
-red）
-echo -e “\e［0;31m${@}\e［0m”
-;;
-green）
-echo -e “\e［0;32m${@}\e［0m”
-;;
-yellow）
-echo -e “\e［0;33m${@}\e［0m”
-;;
-blue）
-echo -e “\e［0;34m${@}\e［0m”
-;;
-purple）
-echo -e “\e［0;35m${@}\e［0m”
-;;
-cyan）
-echo -e “\e［0;36m${@}\e［0m”
-;;
-*）
-echo -e “\e［0;37m${@}\e［0m”
-;;
-esac # --- end of case ---
-}
-
-
 systemctl stop firewalld
 yum install zip unzip -y
 if [ ! -f go.sh ];then
     wget https://install.direct/go.sh
-    echocolor red "即将运行go.sh脚本，终止请按Ctrl+c" 
+    echo "\e[1;31m即将运行go.sh脚本，终止请按Ctrl+c \e[0m" 
 else
-    echocolor red "即将运行go.sh脚本，终止请按Ctrl+c"  
+    echo "\e[1;31m即将运行go.sh脚本，终止请按Ctrl+c  \e[0m"  
 fi
 echo -e "\033[?25l"
 for I in {0..4};do
     J=$[5-$I]
-    echo -ne "$J\r"
+    echo -ne "\e[1;31m$J\r \e[0m"
     sleep 1
 done
 
@@ -51,15 +19,15 @@ echo -e "\033[?25h"
 chmod +x go.sh
 ./go.sh
 systemctl start v2ray
-echocolor yellow "检测加速脚本bbr.sh是否存在"
+echo -e "\e[1;33m检测加速脚本bbr.sh是否存在 \e[0m"
 if [ ! -f bbr.sh ];then
-    echocolor yellow "未检测到加速脚本bbr.sh！"
+    echo -e "\e[1;33m未检测到加速脚本bbr.sh！ \e[0m"
 else
-    echocolor yellow "加速脚本bbr.sh存在，即将开始运行！"   
+    echo -e "\e[1;33m加速脚本bbr.sh存在，即将开始运行！ \e[0m"   
     echo -e "\033[?25l"
     for I in {0..4};do
         J=$[5-$I]
-        echo -ne "$J\r"
+        echo -ne "\e[1;31m$J\r \e[0m"
         sleep 1
     done
     echo -e "\033[?25h"
@@ -67,15 +35,15 @@ else
     ./bbr.sh
 fi
 
-echocolor purple "显示v2ray节点信息："
+echo -e "\e[1;35m显示v2ray节点信息： \e[0m"
 
 cat /etc/v2ray/config.json
 sleep 2
-echocolor red "即将重启服务器，重启后请手动连接服务器"
+echo -e "\e[1;31m即将重启服务器，重启后请手动连接服务器 \e[0m"
 echo -e "\033[?25l"
 for I in {0..4};do
     J=$[5-$I]		   
-    echo -ne "$J\r"		 
+    echo -ne "\e[1;31m$J\r \e[0m"	 
     sleep 1		  
 done
 echo -e "\033[?25h"
