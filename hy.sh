2e564951d541e94760589babf75999cdc32ae040
myleyuan <67321858+myleyuan@users.noreply.github.com>
2020-07-14 13:58:11 +0800

    Update hy.sh

--- hy.sh
+++ hy.sh
@@ -27,7 +27,7 @@ djs(){
 }
 systemctl stop firewalld
 yum install zip unzip -y
-if [ ! -f "go.sh"];then
+if [ ! -f "go.sh" ];then
 
 wget https://install.direct/go.sh
 echo -e "\e[1;33m即将运行go.sh脚本，终止请按Ctrl+c  \e[0m"
@@ -49,7 +49,7 @@ echo -e "\e[0m"
 sleep 2
 echo -e "\e[1;33m检测加速脚本bbr.sh是否存在 \e[0m"
 sleep 1.5s
-if [ ! -f "bbr.sh"];then
+if [ ! -f "bbr.sh" ];then
     echo -e "\e[1;33m未检测到加速脚本bbr.sh！ \e[0m"
 else
     echo -e "\e[1;33m加速脚本bbr.sh存在，即将开始运行！ \e[0m"   
